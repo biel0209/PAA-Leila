f4f3d2e08d0ad644bf364a84a97848d0a44a08c9
Marcus Gabriel <marcus.melo@dcomp.ufs.br>
2022-02-12 15:23:59 -0300

    add q5

---
+++ Lista1/Questao5.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// estrututa nó
+typedef struct no {
+    int conteudo;
+    struct no *esq, *dir;
+} No;
+
+No* inserir(No *raiz, int valor) {
+    if(raiz == NULL) {
+        No *novo = (No*)malloc(sizeof(No));
+        novo->conteudo = valor;
+        novo->esq = NULL;
+        novo->dir = NULL;
+        return novo;
+    } else {
+        if(valor < raiz->conteudo)
+            raiz->esq = inserir(raiz->esq, valor);
+        if(valor > raiz->conteudo)
+            raiz->dir = inserir(raiz->dir, valor);
+        return raiz;
+    }
+}
+
+int isInIntervalo(int x, int a, int b)
+{
+    if (x >= a && x <= b)
+        return x;
+    else    
+        return 0;
+}
+
+int somaArv(No *r, int a, int b)
+{
+    if (r==NULL)
+        return 0;
+    else
+        return isInIntervalo(r->conteudo,a,b) + somaArv(r->esq,a,b) + somaArv(r->dir,a,b);
+}
+
+int main() {
+    int op, valor;
+    No *raiz = NULL;
+    int a = 5; //limite inferior
+    int b = 25; //limite superior
+
+    int v[10] = {0,9,15,92,23,84,22,5,7,3};
+    for(int i=0; i<10; i++)
+       raiz = inserir(raiz, v[i]);// não precisa da estrutura ArvB
+
+    printf("Somatorio: %d\n", somaArv(raiz,a,b));
+    return 0;  
+}
