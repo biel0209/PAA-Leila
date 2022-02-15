1197a34029268c9fb6f804dc249da826a83946f0
Marcus <marcus.melo@dcomp.ufs.br>
2022-02-15 09:27:31 -0300

    att

--- Lista1/Questao6.c
+++ Lista1/Questao6.c
@@ -14,20 +14,20 @@ void selectionSort(int v[], int n)
     }
 }
 
-void concatV3(int v1[], int v2[], int v3[], int n1, int n2)
+void concatV3(int v1[], int v2[], int v3[], int n1, int n2, int n3)
 {
     for(int i=0; i<n1; i++)
         v3[i] = v1[i];
     for(int i=0; i<n2; i++)
         v3[i+n1] = v2[i];
 
-    for(int i=0; i<n1+n2; i++){
-        for(int j=i+1; j<n1+n2; j++){
+    for(int i=0; i<n3; i++){
+        for(int j=i+1; j<n3; j++){
             if (v3[i] == v3[j])
                 v3[j] = -1;
         }
     }
-    selectionSort(v3, n1+n2);
+    selectionSort(v3, n3);
 }
 
 int main()
@@ -36,7 +36,7 @@ int main()
     int v2[5] = {5,7,9,12,15};
     int v3[3+5];
 
-    concatV3(v1,v2,v3,3,5); 
+    concatV3(v1,v2,v3,3,5,3+5); 
 
     for(int i=0; i<3+5; i++) {
         if (v3[i] != -1)
