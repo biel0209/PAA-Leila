02cb0ede704c49ca230dfc48f309a820f8130dfa
biel0209 <52585328+biel0209@users.noreply.github.com>
2022-03-03 11:26:13 -0300

    att

--- Lista2/questao2-solucao1.c
+++ Lista2/questao2-solucao1.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#define TAM 500
-#define LIM 100
+#define TAM 10
+#define LIM 5
 
 typedef struct registro{
     char nome[5];
@@ -54,12 +54,53 @@ void bubbleSort(Registro *vetor)
     }
 }
 
-void maisInfluentes(Registro *reg, Registro *vet)
+int buscarIguais(Registro *vet, int n, int chave, int contador, int indice)
 {
-    bubbleSort(reg);
-    for(int i=0; i<LIM; i++){
-        strcpy(vet[i].nome, reg[i].nome);
-        vet[i].qtd_amigos = reg[i].qtd_amigos;
+    
+    if (contador > n){
+        
+        return indice;
+    }
+    if(vet[n].qtd_amigos == chave){
+        indice=n;
+    }
+    return buscarIguais(vet, n-1, chave, contador, indice);
+}
+
+int ignorarIguais(Registro *vet, int contador, int indice)
+{
+    
+    if (contador <= indice){
+        vet[contador].qtd_amigos = -1;
+        contador++;
+        return ignorarIguais(vet, contador, indice);
+    }else{
+        return 0;
+    }
+}
+
+int maisInfluentes(Registro *reg, Registro *vet, int contador, int contador2)
+{
+    if(contador==LIM)
+        return -1;
+    else{
+        if(contador==0)
+            bubbleSort(reg);
+        if(contador>=3 && reg[contador].qtd_amigos == reg[contador+1].qtd_amigos){
+            int indice = buscarIguais(reg, TAM-1, reg[contador2].qtd_amigos, contador, -1);
+            int sorteado = gerarAleatorio(contador, indice);
+            strcpy(vet[contador].nome, reg[sorteado].nome);
+            vet[contador].qtd_amigos = reg[sorteado].qtd_amigos;
+            //int temp = ignorarIguais(reg, contador, indice);
+            contador2 = indice+1; 
+            
+        }else{
+            strcpy(vet[contador].nome, reg[contador].nome);
+            vet[contador].qtd_amigos = reg[contador].qtd_amigos;
+            contador2++;
+        }
+        contador++;
+        return maisInfluentes(reg, vet, contador, contador2);
     }
 }
 
@@ -70,12 +111,11 @@ int main()
     Registro reg[TAM];
     Registro influentes[LIM];
     preencherVetor(reg);
-    //imprimirVetor(reg, TAM);
-    //printf("\n\n");
-    maisInfluentes(reg, influentes);
+    
+    imprimirVetor(reg, TAM);
+    printf("\n\n");
+    maisInfluentes(reg, influentes,0,0);
+    
     imprimirVetor(influentes, LIM);
-
-    
-    
     return 0;
 }
