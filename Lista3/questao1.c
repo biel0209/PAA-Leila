bc4b81cd58e5524f0db264af65ea7a47c0b035aa
Marcus Gabriel <marcus.melo@dcomp.ufs.br>
2022-03-08 13:57:04 -0300

    att

--- Lista3/questao1.c
+++ Lista3/questao1.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include<time.h>
-#define TAM 10
+#include <time.h>
+#define TAM 8
 
 typedef struct registro{
-    char cpf[11], nome[5], genero;
+    char cpf[12], nome[6], genero;
     int idade;
 }Registro;
 
@@ -49,9 +49,53 @@ void preencherVetor(Registro *vet)
 void imprimirVetor(Registro *vet)
 {
     for (int i = 0; i < TAM; i++){
-        printf("CPF: %s\tNome: %s\ti: %d\tGenero: %c\tIdade: %d\n", 
-                        vet[i].cpf, vet[i].nome, i, 
+        printf("i: %d\tNome: %s \t CPF: %s \tGenero: %c\tIdade: %d\n", 
+                        i+1, vet[i].nome, vet[i].cpf, 
                         vet[i].genero, vet[i].idade);
+    }
+}
+
+void trocar(Registro *vet, int maior, int i)
+{
+    Registro aux;
+    
+    strcpy(aux.cpf, vet[i].cpf);
+    aux.genero = vet[i].genero;
+    aux.idade = vet[i].idade;
+    strcpy(aux.nome, vet[i].nome);
+
+    strcpy(vet[i].cpf, vet[maior].cpf);
+    vet[i].genero = vet[maior].genero;
+    vet[i].idade = vet[maior].idade;
+    strcpy(vet[i].nome, vet[maior].nome);
+
+    strcpy(vet[maior].cpf, aux.cpf);
+    vet[maior].genero = aux.genero;
+    vet[maior].idade = aux.idade;
+    strcpy(vet[maior].nome, aux.nome);
+}
+
+
+void maxHeapify(Registro *vet, int n, int i)
+{
+    int maior;
+    int esq = 2*i, dir = 2*i + 1;
+    if (esq <= n && vet[esq].idade > vet[i].idade)
+        maior = esq;
+    else
+        maior = i;
+    if (dir <= n && vet[dir].idade > vet[maior].idade)
+        maior = dir;
+    if (maior != i){
+        trocar(vet, maior, i);  //trocar A[i] com A[maior]
+        maxHeapify(vet, n, maior);
+    }
+}
+
+void montaMaxHeap(Registro *vet, int n)
+{
+    for(int i = n/2; i>=0; i--){
+        maxHeapify(vet, n, i);
     }
 }
 
@@ -60,9 +104,10 @@ int main()
     srand(time(NULL));
     Registro reg[TAM];
     preencherVetor(reg);
-    printf("%s\n",reg[0].cpf);
     imprimirVetor(reg);
-    
+    montaMaxHeap(reg, TAM);
+    printf("-----HEAP-----\n");
+    imprimirVetor(reg);
     return 0;
 }
 
