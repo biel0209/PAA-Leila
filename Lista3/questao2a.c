7beae47101297915f9b9abb61c215837fb365fb7
biel0209 <52585328+biel0209@users.noreply.github.com>
2022-03-10 10:40:08 -0300

    att

--- Lista3/questao2a.c
+++ Lista3/questao2a.c
@@ -1,1 +1,102 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#define TAM 5
+
+int gerarAleatorio(int inferior, int superior)
+{
+    return inferior + rand() % superior;
+}
+
+void preencherVetor(int *vet)
+{
+    for (int i = 0; i < TAM; i++){
+        vet[i] = gerarAleatorio(0, 100);
+    }
+}
+
+void imprimirVetor(int *vet, int n)
+{
+    for (int i = 0; i < n; i++){
+        printf("i: %d\tElemento: %d\n", i+1, vet[i]);
+    }
+}
+
+void trocar(int *vet, int a, int b)
+{
+    int aux;
+    aux = vet[b];
+    vet[b] = vet[a];
+    vet[a] = aux;
+}
+
+void maxHeapify(int *vet, int n, int i)
+{
+    int maior = i;
+    int esq = (2*i)+1;
+    int dir = (2*i)+2;
+    if (esq < n && vet[esq] > vet[i])
+            maior = esq;
+
+    if (dir < n && vet[dir] > vet[maior])
+            maior = dir;
+   
+    if (maior != i){
+        trocar(vet, maior, i);  
+        maxHeapify(vet, n, maior);
+    }
+}
+
+void montaMaxHeap(int *vet, int n)
+{
+    for(int i = n/2 - 1; i>=0; i--){
+        maxHeapify(vet, n, i);
+    }
+}
+
+void rearranjeMaxHeap(int *vet, int n)
+{
+    int pai = 0, filho = 1;
+    while(filho <= n-1){
+        if(vet[filho] < vet[filho+1]) filho++;
+        if(vet[filho] > vet[pai]){
+            trocar(vet, filho, pai);
+            pai = filho;
+            filho = 2*filho;
+        }else{
+            filho = n;
+        }
+    }
+}
+
+void removeMaxHeap(int *vet, int n)
+{
+    if (n==0)
+        printf("heap vazio!");
+    else{
+        trocar(vet, 0, n-1);
+        n--;
+        rearranjeMaxHeap(vet, n);
+    }
+}
+
+
+int main()
+{
+    //srand(time(NULL));
+    int reg[TAM];
+    preencherVetor(reg);
+    montaMaxHeap(reg, TAM);
+    printf("-----HEAP-----\n");
+    imprimirVetor(reg, TAM);
+    
+    printf("-----REMOCAO-----\n");
+    removeMaxHeap(reg, TAM);
+    imprimirVetor(reg, TAM-1);
+    return 0;
+}
+
+
+ 
