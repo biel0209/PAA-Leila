8d81514b9ddf6873de4ae2f0fd2d357be4f41cdd
Marcus <marcus.melo@dcomp.ufs.br>
2022-03-16 00:02:32 -0300

    att

---
+++ Lista1-Opcional/sumOfCubes.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#define TAM 20
+
+int main()
+{
+    //int a=2;
+    //int b=3;
+    //int x = 16;
+    /*
+    Se x=16 e a = 1, no máximo, b = raiz cubica de x - 1 
+    vet[b] 
+    */
+   int a=1;
+   int x = 
+   int b = pow(x, 1.0/3.0) - 1;
+   for(int i=1; i<=b; i++){
+
+   }
+    int b = pow(8, 1.0/3.0);
+    printf("%d\n",b);
+    
+    return 0;
+}
+
+
+ 
