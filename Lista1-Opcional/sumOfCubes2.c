54252a5205343c6d07afd5bc6d42bae8d6bc2d6f
Marcus Gabriel <marcus.melo@dcomp.ufs.br>
2022-03-18 14:16:52 -0300

    att

--- Lista1-Opcional/sumOfCubes2.c
+++ Lista1-Opcional/sumOfCubes2.c
@@ -1,29 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/time.h>
-#include <time.h>
 #include <math.h>
  
-int buscar(long long int chave, long long int vet[], long long int x)
+int testarCubos(long long int x)
 {
-    
-    for(long a=0; (a*a*a)<x; a++){
-        if(vet[a] == chave)
+    long a = 1;
+    long b = (long)pow(x, 1.0/3.0);
+    while(a <= b){
+        long long int atual = a*a*a + b*b*b;
+        if(atual == x)
             return 1;
-    }
-    return 0;
-}
- 
-int teste(long long int x)
-{
-    long limite = pow(x, 1.0/3.0)+1;
-    long long int vet[limite];
-    for(long a=1; a<=limite; a++)
-        vet[a-1] = (a*a*a);
-    for(long b=1; b<=limite; b++){
-        if( buscar(x-(b*b*b), vet, x) )
-            return 1;
+        if(atual < x)
+            a++;
+        else
+            b--;
     }
     return 0;
 }
@@ -36,7 +26,7 @@ int main()
     int vet[qtd_testes];
     for(int i=0; i<qtd_testes; i++){
         scanf("%lld",&x);
-        if(teste(x))
+        if(testarCubos(x))
             vet[i] = 1; 
         else
             vet[i] = 0; 
