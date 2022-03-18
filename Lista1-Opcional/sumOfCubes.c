f08f735a0f82eebab500f060a3683a130683b3e6
Marcus Gabriel <marcus.melo@dcomp.ufs.br>
2022-03-18 13:36:10 -0300

    att

--- Lista1-Opcional/sumOfCubes.c
+++ Lista1-Opcional/sumOfCubes.c
@@ -1,54 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/time.h>
 #include <time.h>
 #include <math.h>
-
-int buscar(long chave, long *vet, long x)
+ 
+int buscar(long long int chave, long long int vet[], long long int x)
 {
-    for(long a=0; a*a*a<x; a++){
+    
+    for(long a=0; (a*a*a)<x; a++){
         if(vet[a] == chave)
             return 1;
     }
     return 0;
 }
-
-int teste(long x)
+ 
+int teste(long long int x)
 {
-    int limite = pow(x, 1.0/3.0)+1;
-    long vet[limite];
-    for(long a=1; a*a*a<x; a++)
-        vet[a] = a*a*a;
-    for(long b=1; b*b*b<x; b++){
+    long limite = pow(x, 1.0/3.0)+1;
+    long long int vet[limite];
+    for(long a=1; a<=limite; a++)
+        vet[a-1] = (a*a*a);
+    for(long b=1; b<=limite; b++){
         if( buscar(x-(b*b*b), vet, x) )
             return 1;
     }
-    
     return 0;
 }
-
+ 
 int main()
 {
-    clock_t t = clock();
-
-    //Algoritmo
     int qtd_testes;
-    long x;
+    long long int x;
     scanf("%d",&qtd_testes);
+    int vet[qtd_testes];
     for(int i=0; i<qtd_testes; i++){
-        scanf("%ld",&x);
+        scanf("%lld",&x);
         if(teste(x))
+            vet[i] = 1; 
+        else
+            vet[i] = 0; 
+    }
+    //dfdfg
+ 
+    
+    for(int i=0; i<qtd_testes; i++){
+        if(vet[i] == 1)
             printf("YES\n");
         else
             printf("NO\n");
-
     }
-    //Algoritmo
     
-    double Tempo = (clock() - t) * 1000.0 / CLOCKS_PER_SEC;
-    printf("Tempo gasto: %g ms.\n", Tempo);
+    
     return 0;
+    
 }
-
-
- 
