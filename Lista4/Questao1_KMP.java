b1264d90dba2771245c863342670a7e32c1fbc0e
Marcus Gabriel <marcus.melo@dcomp.ufs.br>
2022-03-21 23:17:53 -0300

    att

--- Lista4/Questao1_KMP.java
+++ Lista4/Questao1_KMP.java
@@ -2,7 +2,7 @@ package Lista4;
 
 public class Questao1_KMP{
     public static void main(String[] args){
-        String padrao = "marmar";
+        String padrao = "mar"; 
         String texto1 = "marararmarmararmar";
         String texto2 = "marmarararmarararmarmarmarmararar";
         String texto3 = "marararmarmararmarmararmarmar";
@@ -10,33 +10,50 @@ public class Questao1_KMP{
         System.out.println(resp);
     }
 
-    public static void computaNext(String P, int m, int[] next){
+    /*public static void computaNext(String P, int m, int next[]){
+        next[0] = -1;
+        next[1] = 0;
+        int i;
+        for(i=2; i<m; i++){
+            int j = next[i-1];
+            while(P.charAt(i-1) != P.charAt(j) && j>0)
+                j = next[j];
+            next[i] = j;
+        }
+    }*/
+
+    public static void computaNext(String P, int m, int next[]){
         next[0] = 0;
-        int k=0;
-        for(int i=1; i<m; i++){
-            while(P.charAt(k+1) != P.charAt(i) && k>0){
-                k = next[k];
+        int n=0;
+        int i=1;
+        while(i<m){
+            if(P.charAt(i) == P.charAt(n)){
+                t++;
             }
-            if (P.charAt(k+1) == P.charAt(i))
-                k++;
-            next[i] = k;
         }
     }
 
     public static int KMP(String T, int n, String P, int m){
+        int cont=0;
         int next[] = new int[m];
         computaNext(P, m, next);
-        int q=0, index = -1;
-        for(int i=1; i<n; i++){
-            while(P.charAt(q+1) != T.charAt(i) && q>0)
-                q = next[q];
-            if (P.charAt(q+1) == T.charAt(i))
-                q++;
-            if(q == m){
+        int i=0, j=0, index = -1;
+        while(index == -1 && i<n){
+            if( P.charAt(j) == T.charAt(i)){
+                j++;
+                i++;
+            }else{
+                if(j==0){
+                    i++;
+                }else{
+                    j = next[j-1];
+                }
+            }if (j == m){
+                cont++;
                 index = i-m;
-                q = next[q];
+                j = next[j-1];
             }
         }
-        return index;
+        return cont;
     }
 }   
