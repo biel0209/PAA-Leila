82bcab2d44439d2b3399396d0d5796924f0e5c32
biel0209 <52585328+biel0209@users.noreply.github.com>
2022-03-23 09:39:37 -0300

    att

--- Lista4/Questao2.java
+++ Lista4/Questao2.java
@@ -12,36 +12,26 @@ public class Main{
     }
 
     public static void buscarOcorrencias(String T, int n, String P[], int m){
-        //Busca de cada quantidade de ocorrencias de P em cada Texto
-        Indice index[] = new Indice[n];
-        for(int i=0; i<n; i++){
-            index[i] = new Indice();
-            index[i].quantidade = Rabin_Karp(T[i], T[i].length(), P, m);
-            index[i].texto = i;
-        }
-
- 
-
-        System.out.println("Texto(s) mais relevante(s):");
-        //Buscar repetidos
-        for(int i=0; i<m; i++){
-            System.out.println("Texto de indice " + index[i].texto +
-                                " Quantidade de ocorrencias: " +
-                                index[i].quantidade);
+        int q = 3354393, d = 32;
+        
+        //Pre processamento do texto
+        int h2=0;
+        for (int i=0; i < m; i++)  
+            h2 = (h2*d + T.charAt(i))%q;
             
+        //Busca da quantidade de ocorrencias de cada P no Texto
+        for(int i=0; i<m; i++){
+            /*int qtd =*/ Rabin_Karp(T, n, P[i], m, q, d, h2);
+            /*System.out.println("Padrao de indice " + i +
+                    " Quantidade de ocorrencias no texto: " +
+                    quantidade);*/
         }
     }
 
-    public static void preProcess(String T, int n, String P, int m, int q, int d){
-        int h1 = 0, h2 = 0, dM = 1;
+    public static void Rabin_Karp(String T, int n, String P, int m, int q, int d, int h2){
+        int h1 = 0, dM = 1;
         for(int i=0; i < m-1; i++)
             dM = (dM*d)%q;
-        for (int i=0; i < m; i++)  //Pre processamento do texto
-            h2 = (h2*d + T.charAt(i))%q;
-        
-    }
-
-    public static int Rabin_Karp(String T, int n, String P, int m, int q, int d, int h1, int dM){
         for (int i=0; i < m; i++) //Pre processamento do padrao
             h1 = (h1*d + P.charAt(i))%q;
         for (int i=0; i < m; i++){
