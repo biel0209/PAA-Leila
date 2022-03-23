ea4d96dddd3932a41891cd367817eaa27e4441ae
biel0209 <52585328+biel0209@users.noreply.github.com>
2022-03-23 09:30:58 -0300

    att

--- Lista4/Questao2.java
+++ Lista4/Questao2.java
@@ -1,73 +1,58 @@
-package Lista4;
+//package Lista4;
 
-class Indice{
-    int quantidade;
-    int texto;
-}
-
-public class Questao1_Horspool{
+public class Main{
     public static void main(String[] args){
-        String padrao = "mar";
-        int n = 4; 
-        String [] texto = new String[n];
-        texto[0] = "marararmarmararmar";
-        texto[1] = "marararmarararmarmarmarmararar";
-        texto[2] = "ararmarmarmarmararmarmararmarmar";
-        texto[3] = "ararmarmarmararararmararmarararmarmarararmar";;
-        maisRelevante(texto, texto.length, padrao, padrao.length());
+        String texto = "ararammarmarmararararmararmarararammarmarararmar";
+        int p = 3; 
+        String [] padrao = new String[p];
+        padrao[0] = "mar";
+        padrao[1] = "arm";
+        padrao[2] = "ram";
+        buscarOcorrencias(texto, texto.length(), padrao, padrao.length);
     }
 
-    public static void maisRelevante(String T[], int n, String P, int m){
+    public static void buscarOcorrencias(String T, int n, String P[], int m){
         //Busca de cada quantidade de ocorrencias de P em cada Texto
         Indice index[] = new Indice[n];
         for(int i=0; i<n; i++){
             index[i] = new Indice();
-            index[i].quantidade = Horspool(T[i], T[i].length(), P, m);
+            index[i].quantidade = Rabin_Karp(T[i], T[i].length(), P, m);
             index[i].texto = i;
         }
 
-        //Buscar o(s) texto(s) mais relevante(s)
-        Indice maior = new Indice();
-        maior.quantidade = index[0].quantidade;
-        maior.texto = index[0].texto;
-        for(int i=0; i<n; i++){
-            if(maior.quantidade < index[i].quantidade){
-                maior.quantidade = index[i].quantidade;
-                maior.texto = index[i].texto;
-            }
-        }
+ 
 
         System.out.println("Texto(s) mais relevante(s):");
         //Buscar repetidos
-        for(int i=0; i<n; i++){
-            if(maior.quantidade == index[i].quantidade){
-                System.out.println("Texto de indice " + index[i].texto +
-                                    " Quantidade de ocorrencias: " +
-                                    index[i].quantidade);
+        for(int i=0; i<m; i++){
+            System.out.println("Texto de indice " + index[i].texto +
+                                " Quantidade de ocorrencias: " +
+                                index[i].quantidade);
+            
+        }
+    }
+
+    public static void preProcess(String T, int n, String P, int m, int q, int d){
+        int h1 = 0, h2 = 0, dM = 1;
+        for(int i=0; i < m-1; i++)
+            dM = (dM*d)%q;
+        for (int i=0; i < m; i++)  //Pre processamento do texto
+            h2 = (h2*d + T.charAt(i))%q;
+        
+    }
+
+    public static int Rabin_Karp(String T, int n, String P, int m, int q, int d, int h1, int dM){
+        for (int i=0; i < m; i++) //Pre processamento do padrao
+            h1 = (h1*d + P.charAt(i))%q;
+        for (int i=0; i < m; i++){
+            if(h1 == h2)
+                System.out.println("Pattern found at index " + i);
+            if(i < n-m){
+                h2 = (h2 + (d*q - T.charAt(i)*dM))%q;
+                if(h2<0)
+                    h2 = h2+q;
             }
+            
         }
-    }
-
-    public static void computaDesloc(String P, int m, int D[], int tam){
-        for(int j=0; j<tam; j++)
-            D[j] = m;
-        for(int j=0; j<=m-2; j++)
-            D[P.charAt(j)] = m-1-j;
-    }
-
-    public static int Horspool(String T, int n, String P, int m){
-        int qtdOcorrencias=0;
-        int D[] = new int[256];
-        computaDesloc(P, m, D, 256);
-        int i=m-1;
-        while(i<=n-1){
-            int k=0;
-            while(k<=m-1 && P.charAt(m-1-k) == T.charAt(i-k))
-                k++;
-            if(k==m)
-                qtdOcorrencias++;
-            i = i + D[T.charAt(i)];
-        }
-        return qtdOcorrencias;
     }
 }   
