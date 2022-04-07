e09a48fb943f659ac981d5034aa077f06fde9679
Marcus <marcus.melo@dcomp.ufs.br>
2022-04-07 11:17:47 -0300

    att

--- Lista5/LCS.java
+++ Lista5/LCS.java
@@ -8,18 +8,22 @@ class Matriz{
 public class LCS {
     public static void main(String[] args){
         String str1 = "abcdfg";
-        String str2 = "adf";
+        String str2 = "acg";
         char[] X = str1.toCharArray();
         char[] Y = str2.toCharArray();
         int m = X.length;
         int n = Y.length;
         Matriz LCS[][] = new Matriz[m+1][n+1];
+        inicializarMatriz(LCS,m,n);
+        maiorSequencia(X, m, Y, n, LCS);
+    }
+
+    public static void inicializarMatriz(Matriz[][] LCS, int m, int n){
         for(int i=0; i<m+1; i++){
             for(int j=0; j<n+1; j++){
                 LCS[i][j] = new Matriz();
             }
         }
-        maiorSequencia(X, m, Y, n, LCS);
     }
 
     public static void maiorSequencia(char[] X, int m, char[] Y, int n, Matriz[][] LCS){
@@ -35,11 +39,9 @@ public class LCS {
             LCS[0][j].dir = '*';
         }
 
-        
-
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                if(X[i] == Y[j]){
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(X[i-1] == Y[j-1]){
                     LCS[i][j].tam = 1 + LCS[i-1][j-1].tam;
                     LCS[i][j].dir = 'D';
                 }else{
